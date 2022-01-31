268d7d9cb3a8930d6e113727d37e01fee28a8b10
sridharanv2016 <96345468+sridharanv2016@users.noreply.github.com>
2022-01-31 23:49:02 +0530

    Add files via upload

---
+++ BMI/automation/test_BMI.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+sys.path.insert(0, '../code')
+import BMI
+from pathlib import Path
+import pandas as pd
+
+
+class TestBMI(unittest.TestCase):
+    filename = Path("../data/data.json")
+    lookupfilename = Path("../data/lookup.json")
+
+    def test_fileexists(self):
+        result = BMI.file_name_exists()
+        self.assertEqual(result,1)
+
+    def test_get_body_mass_index(self):
+        df = pd.DataFrame({
+            'WeightKg': [80],
+            'HeightCm':  [140],
+        })
+        result = df.apply(BMI.get_body_mass_index, axis=1)
+        self.assertTrue(result[0] > 0)
+
+    def test_read_json_into_df(self):
+        result = BMI.read_json_into_df()
+        print(result)
+        self.assertTrue('ERROR' not in result)
+
+if  __name__ == '__main__':
+    unittest.main()
